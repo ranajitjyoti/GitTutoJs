1fd11c2c1506c89952b1d9fe39dd4dca8c799ed7
ranajitjyoti <ranajitjyoti1@gmail.com>
2022-09-05 00:40:46 +0530

    commit 2

--- GitTuto.js
+++ GitTuto.js
@@ -2,3 +2,5 @@ var a=2
 var b=4
 console.log(a+b)
 console.log("java script is runnig")
+var k="ranajit";
+var i=4;
--- GitTuto2.js
+++ GitTuto2.js
@@ -4,3 +4,4 @@ for(let i=0;i<=k;i++)
 {
     console.log("this is a for loop",i)
 }
+let j="uuu"
